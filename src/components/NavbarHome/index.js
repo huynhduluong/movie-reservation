7d2cc820c06a1ddcb6aa447b07d4c0af0fc596c7
Huynh Du Luong <huynhduluong@gmail.com>
2020-11-24 18:50:23 +0700

    handle carousel homepage

--- src/components/NavbarHome/index.js
+++ src/components/NavbarHome/index.js
@@ -16,54 +16,52 @@ import { useStyles } from "./NavbarStyle";
 export default function NavbarHome() {
   const classes = useStyles();
   return (
-    <React.Fragment>
+    <div>
       <div className={classes.maxHeightNavbarHome}></div>
-      <div>
-        <AppBar position="fixed" className={classes.root}>
-          <Toolbar>
-            <Avatar
-              alt="logo"
-              variant="square"
-              src="img/logo.png"
-              className={classes.logo}
-            ></Avatar>
-            <div className={classes.listNav}>
-              <Hidden xsDown>
-                <Box display="flex">
-                  <Link href="#" className={classes.link}>
-                    Lịch chiếu
-                  </Link>
-                  <Link href="#" className={classes.link}>
-                    Cụm rạp
-                  </Link>
-                  <Link href="#" className={classes.link}>
-                    Tin tức
-                  </Link>
-                  <Link href="#" className={classes.link}>
-                    Ứng dụng
-                  </Link>
-                </Box>
-              </Hidden>
-            </div>
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              className={classes.navRight}
-            >
-              <Hidden smDown>
-                <Link to="/dang-nhap" className={classes.textLogin}>
-                  <UserIcon />
+      <AppBar position="fixed" className={classes.root}>
+        <Toolbar>
+          <Avatar
+            alt="logo"
+            variant="square"
+            src="img/logo.png"
+            className={classes.logo}
+          ></Avatar>
+          <div className={classes.listNav}>
+            <Hidden xsDown>
+              <Box display="flex">
+                <Link href="#" className={classes.link}>
+                  Lịch chiếu
                 </Link>
-                <SwitchMode />
-              </Hidden>
-              <Hidden mdUp>
-                <ToggleMenu />
-              </Hidden>
-            </Box>
-          </Toolbar>
-        </AppBar>
-      </div>
-    </React.Fragment>
+                <Link href="#" className={classes.link}>
+                  Cụm rạp
+                </Link>
+                <Link href="#" className={classes.link}>
+                  Tin tức
+                </Link>
+                <Link href="#" className={classes.link}>
+                  Ứng dụng
+                </Link>
+              </Box>
+            </Hidden>
+          </div>
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            className={classes.navRight}
+          >
+            <Hidden smDown>
+              <Link to="/dang-nhap" className={classes.textLogin}>
+                <UserIcon />
+              </Link>
+              <SwitchMode />
+            </Hidden>
+            <Hidden mdUp>
+              <ToggleMenu />
+            </Hidden>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </div>
   );
 }
