44401b031d81e63926c05f7ed650dfd1954ad44d
Huynh Du Luong <huynhduluong@gmail.com>
2021-01-28 06:17:20 +0700

    create layout navbarAdmin  and handle logout admin

--- src/components/NavbarAdmin/index.js
+++ src/components/NavbarAdmin/index.js
@@ -1,9 +1,245 @@
+import {
+  AppBar,
+  Badge,
+  IconButton,
+  Toolbar,
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  Drawer,
+  Avatar,
+  Box,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import PeopleIcon from "@material-ui/icons/People";
+import MovieIcon from "@material-ui/icons/Movie";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import MoreIcon from "@material-ui/icons/MoreVert";
 import React from "react";
+import clsx from "clsx";
+import Logo from "./../../assets/img/logo.png";
+import { Link } from "react-router-dom";
+import { NavbarAdminStyles } from "./NavbarAdminStyles";
 
-export default function NavbarAdmin() {
+export default function NavbarAdmin(props) {
+  const classes = NavbarAdminStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("UserAdmin");
+    props.history.push("/auth");
+  };
+
+  // handle icon message, notification, user (logout)
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Thông tin</MenuItem>
+      <MenuItem onClick={handleLogout}>Đăng xuất</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      {/* <MenuItem>
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Tin nhắn</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Thông báo</p>
+      </MenuItem> */}
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Đăng xuất</p>
+      </MenuItem>
+    </Menu>
+  );
+  // End menu
+
   return (
-    <div>
-      <h1>NavbarAdmin</h1>
-    </div>
+    <React.Fragment>
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden
+            )}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.titlePage}
+          >
+            Dashboard
+          </Typography>
+
+          {/* Menu for user, notification, message */}
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            {/* <Typography component="h3" variant="h6" color="inherit" noWrap>
+              Chào, {JSON.parse(localStorage.getItem("UserAdmin")).hoTen}
+            </Typography> */}
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+          {/* End menu */}
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+      <Drawer
+        variant="permanent"
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbarIcon}>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Avatar alt="Logo" src={Logo} />
+            <Typography component="h1" variant="h6" color="inherit" noWrap>
+              Square
+            </Typography>
+          </Box>
+          <IconButton onClick={handleDrawerClose}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem button component={Link} to="/dashboard">
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+          <ListItem button component={Link} to="/user-admin">
+            <ListItemIcon>
+              <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary="User Management" />
+          </ListItem>
+          <ListItem button component={Link} to="/film-admin">
+            <ListItemIcon>
+              <MovieIcon />
+            </ListItemIcon>
+            <ListItemText primary="Film Management" />
+          </ListItem>
+        </List>
+        <Divider />
+      </Drawer>
+    </React.Fragment>
   );
 }
--- src/containers/AdminTemplate/index.js
+++ src/containers/AdminTemplate/index.js
@@ -1,13 +1,16 @@
+import { Box } from "@material-ui/core";
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import NavbarAdmin from "../../components/NavbarAdmin";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 function AdminLayout(props) {
   return (
-    <div>
-      <NavbarAdmin />
+    <Box display="flex">
+      <CssBaseline />
+      <NavbarAdmin history={props.children.props.history} />
       {props.children}
-    </div>
+    </Box>
   );
 }
 
